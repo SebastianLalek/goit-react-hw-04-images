7df1d5c8490ea8b958e398202dfd1c61703acc80
Sebastian Lalek <chemiczq@gmail.com>
2023-11-19 18:51:11 +0100

    refactor query searching and pagination

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Searchbar from './searchbar/Seachbar';
 import ImageGallery from './imagegallery/ImageGallery';
 import ImageGalleryItem from './imagegalerryitem/ImageGalleryItem';
@@ -8,115 +8,207 @@ import Loader from './loader/Loader';
 import Button from './button/Button';
 import Modal from './modal/Modal';
 
-class ImageFinder extends Component {
-  state = {
-    isLoading: false,
-    modalOn: false,
-    modalImage: '',
-    query: '',
-    pictures: [],
-    totalPictures: 0,
-    currentPage: 1,
-    error: '',
-  };
+function ImageFinder() {
+  const [pictures, setPictures] = useState([]);
+  const [modalOn, setModalOn] = useState(false);
+  const [isLoading, setisLoading] = useState(false);
+  const [totalPictures, setTotalPictures] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [error, setError] = useState('');
 
-  getImages = async () => {
+  const getImages = async (query, page) => {
     const URL = 'https://pixabay.com/api/';
 
     const params = {
       key: '39310150-ae6655c0c5d929a5c6e93be30',
-      q: this.state.query,
+      q: query,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-      page: this.state.currentPage,
+      page: page,
       per_page: 12,
     };
 
+    if (query === '') {
+      return setPictures([]);
+    }
+
     try {
-      this.setState({ isLoading: true });
+      setisLoading(true);
 
       const result = await axios
         .get(URL, { params })
         .then(response => response.data);
 
-      this.setState({ totalPictures: result.totalHits });
+      const totalHits = result.totalHits;
+
+      setTotalPictures(totalHits);
 
       const photos = await result.hits;
 
-      this.setState(state => ({
-        pictures: [...state.pictures, ...photos],
-      }));
+      setPictures(pictures => [...pictures, ...photos]);
     } catch (error) {
-      this.setState({ error: error.toString() });
+      setError(error.toString());
     } finally {
-      this.setState({ isLoading: false });
+      setisLoading(false);
     }
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const newState = this.state;
-
-    if (
-      prevState.currentPage !== newState.currentPage ||
-      prevState.query !== newState.query
-    ) {
-      this.getImages();
-    }
-  }
-
-  handleSubmit = async e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const form = e.currentTarget;
-    const query = form.elements.query.value;
 
-    this.setState({
-      query: query,
-      pictures: [],
-      currentPage: 1,
-    });
+    const query = e.target.query.value;
+
+    setSearchQuery(query);
+
+    setPictures([]);
+    setCurrentPage(1);
   };
 
-  handleClick = () => {
-    this.setState(state => ({ currentPage: state.currentPage + 1 }));
+  useEffect(() => {
+    getImages(searchQuery, currentPage);
+  }, [searchQuery, currentPage]);
+
+  const imageHandler = e => {
+    console.log(e);
+  };
+  const modalImage = e => {
+    console.log(e);
+  };
+  const handleClick = () => {
+    setCurrentPage(currentPage + 1);
+  };
+  const modalHandler = e => {
+    console.log(e);
   };
 
-  imageHandler = e => {
-    const largeImage = e.target.dataset.image;
-
-    if (largeImage === undefined) {
-      return;
-    }
-
-    this.setState({ modalImage: largeImage, modalOn: true });
-  };
-
-  modalHandler = e => {
-    if (e.target.className === 'Overlay') {
-      this.setState({ modalOn: false });
-    }
-  };
-
-  render() {
-    const { pictures, totalPictures, isLoading, modalOn, modalImage } =
-      this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSubmit} />
-        <ImageGallery onClick={this.imageHandler}>
-          {pictures.map(picture => (
-            <ImageGalleryItem key={picture.id} image={picture} />
-          ))}
-        </ImageGallery>
-        {modalOn && <Modal imageSrc={modalImage} onClick={this.modalHandler} />}
-        {isLoading && <Loader />}
-        {pictures.length !== 0 && pictures.length !== totalPictures && (
-          <Button onClick={this.handleClick} />
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      <ImageGallery onClick={imageHandler}>
+        {pictures.map(picture => (
+          <ImageGalleryItem key={picture.id} image={picture} />
+        ))}
+      </ImageGallery>
+      {modalOn && <Modal imageSrc={modalImage} onClick={modalHandler} />}
+      {isLoading && <Loader />}
+      {pictures.length !== totalPictures && pictures.length !== 0 && (
+        <Button onClick={handleClick} />
+      )}
+    </>
+  );
 }
+
+// class ImageFinder extends Component {
+//   state = {
+//     isLoading: false,
+//     modalOn: false,
+//     modalImage: '',
+//     query: '',
+//     pictures: [],
+//     totalPictures: 0,
+//     currentPage: 1,
+//     error: '',
+//   };
+
+//   getImages = async () => {
+//     const URL = 'https://pixabay.com/api/';
+
+//     const params = {
+//       key: '39310150-ae6655c0c5d929a5c6e93be30',
+//       q: this.state.query,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       safesearch: true,
+//       page: this.state.currentPage,
+//       per_page: 12,
+//     };
+
+//     try {
+//       this.setState({ isLoading: true });
+
+//       const result = await axios
+//         .get(URL, { params })
+//         .then(response => response.data);
+
+//       this.setState({ totalPictures: result.totalHits });
+
+//       const photos = await result.hits;
+
+//       this.setState(state => ({
+//         pictures: [...state.pictures, ...photos],
+//       }));
+//     } catch (error) {
+//       this.setState({ error: error.toString() });
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const newState = this.state;
+
+//     if (
+//       prevState.currentPage !== newState.currentPage ||
+//       prevState.query !== newState.query
+//     ) {
+//       this.getImages();
+//     }
+//   }
+
+//   handleSubmit = async e => {
+//     e.preventDefault();
+//     const form = e.currentTarget;
+//     const query = form.elements.query.value;
+
+//     this.setState({
+//       query: query,
+//       pictures: [],
+//       currentPage: 1,
+//     });
+//   };
+
+//   handleClick = () => {
+//     this.setState(state => ({ currentPage: state.currentPage + 1 }));
+//   };
+
+//   imageHandler = e => {
+//     const largeImage = e.target.dataset.image;
+
+//     if (largeImage === undefined) {
+//       return;
+//     }
+
+//     this.setState({ modalImage: largeImage, modalOn: true });
+//   };
+
+//   modalHandler = e => {
+//     if (e.target.className === 'Overlay') {
+//       this.setState({ modalOn: false });
+//     }
+//   };
+
+//   render() {
+//     const { pictures, totalPictures, isLoading, modalOn, modalImage } =
+//       this.state;
+//     return (
+//       <>
+//         <Searchbar onSubmit={this.handleSubmit} />
+//         <ImageGallery onClick={this.imageHandler}>
+//           {pictures.map(picture => (
+//             <ImageGalleryItem key={picture.id} image={picture} />
+//           ))}
+//         </ImageGallery>
+//         {modalOn && <Modal imageSrc={modalImage} onClick={this.modalHandler} />}
+//         {isLoading && <Loader />}
+//         {pictures.length !== 0 && pictures.length !== totalPictures && (
+//           <Button onClick={this.handleClick} />
+//         )}
+//       </>
+//     );
+//   }
+// }
 
 export const App = () => {
   return (
--- src/consts/pixabay.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = 'https://pixabay.com/api/';
-export const API_KEY = process.env.REACT_APP_API_KEY;
